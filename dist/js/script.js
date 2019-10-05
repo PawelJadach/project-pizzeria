5c9327f9989ea4cebb231fa8eb1586b4c9437fa4
unknown <pawel1234477@wp.pl>
2019-10-05 11:39:22 +0200

    Add toggle cart

--- dist/js/script.js
+++ dist/js/script.js
@@ -273,6 +273,7 @@
       const thisCart = this;
       thisCart.products = [];
       thisCart.getElements(element);
+      thisCart.initActions();
       console.log('new Cart', thisCart);
     }
 
@@ -280,6 +281,15 @@
       const thisCart = this;
       thisCart.dom = {};
       thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+
+    }
+
+    initActions(){
+      const thisCart = this;
+      thisCart.dom.toggleTrigger.addEventListener('click', function(){
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
     }
   }
 
