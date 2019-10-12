185c4b297487c40b9bf348598c4f7c26cb2cf803
unknown <pawel1234477@wp.pl>
2019-10-12 12:47:21 +0200

    Add code for 10 module

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,11 +1,16 @@
+/* global Handlebars */
+
 export const select = {
   templateOf: {
     menuProduct: '#template-menu-product',
     cartProduct: '#template-cart-product',
+    bookingWidget: '#template-booking-widget',
   },
   containerOf: {
     menu: '#product-list',
     cart: '#cart',
+    pages: '#pages',
+    booking: '.booking-wrapper',
   },
   all: {
     menuProducts: '#product-list > .product',
@@ -26,6 +31,15 @@ export const select = {
       linkDecrease: 'a[href="#less"]',
       linkIncrease: 'a[href="#more"]',
     },
+    datePicker: {
+      wrapper: '.date-picker',
+      input: `input[name="date"]`,
+    },
+    hourPicker: {
+      wrapper: '.hour-picker',
+      input: 'input[type="range"]',
+      output: '.output',
+    },
   },
   cart: {
     productList: '.cart__order-summary',
@@ -45,6 +59,14 @@ export const select = {
     edit: '[href="#edit"]',
     remove: '[href="#remove"]',
   },
+  booking: {
+    peopleAmount: '.people-amount',
+    hoursAmount: '.hours-amount',
+    tables: '.floor-plan .table',
+  },
+  nav: {
+    links: '.main-nav a',
+  },
 };
 
 export const classNames = {
@@ -55,27 +77,52 @@ export const classNames = {
   cart: {
     wrapperActive: 'active',
   },
+  booking: {
+    loading: 'loading',
+    tableBooked: 'booked',
+  },
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  },
 };
 
 export const settings = {
+  hours: {
+    open: 12,
+    close: 24,
+  },
   amountWidget: {
     defaultValue: 1,
     defaultMin: 1,
     defaultMax: 9,
   },
+  datePicker: {
+    maxDaysInFuture: 14,
+  },
   cart: {
     defaultDeliveryFee: 20,
+  },
+  booking: {
+    tableIdAttribute: 'data-table',
   },
   db: {
     url: '//localhost:3131',
     product: 'product',
     order: 'order',
+    booking: 'booking',
+    event: 'event',
+    dateStartParamKey: 'date_gte',
+    dateEndParamKey: 'date_lte',
+    notRepeatParam: 'repeat=false',
+    repeatParam: 'repeat_ne=false',
   },
 };
 
 export const templates = {
-  /* eslint-disable */
   menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+  bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
 };
-
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,4 +1,7 @@
-const utils = {}; // eslint-disable-line no-unused-vars
+/* global Handlebars, dataSource */
+
+export const utils = {};
+
 utils.createDOMFromHTML = function(htmlString) {
   let div = document.createElement('div');
   div.innerHTML = htmlString.trim();
@@ -13,7 +16,7 @@ utils.createPropIfUndefined = function(obj, key, value = []){
 
 utils.serializeFormToObject = function(form){
   let output = {};
-  if (typeof form == 'object' && form.nodeName == 'FORM'){
+  if (typeof form == 'object' && form.nodeName == 'FORM') {
     for (let field of form.elements) {
       if (field.name && !field.disabled && field.type != 'file' && field.type != 'reset' && field.type != 'submit' && field.type != 'button') {
         if (field.type == 'select-multiple') {
@@ -33,15 +36,41 @@ utils.serializeFormToObject = function(form){
   return output;
 };
 
-// utils.convertDataSourceToDbJson = function(){
-//   const productJson = [];
-//   for(let key in dataSource.products){
-//     productJson.push(Object.assign({id: key}, dataSource.products[key]));
-//   }
+utils.queryParams = function(params){
+  return Object.keys(params)
+    .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+    .join('&');
+};
 
-//   console.log(JSON.stringify({product: productJson, order: []}, null, '  '));
-// };
-/* eslint-disable */
+utils.convertDataSourceToDbJson = function(){
+  const productJson = [];
+  for(let key in dataSource.products){
+    productJson.push(Object.assign({id: key}, dataSource.products[key]));
+  }
+
+  console.log(JSON.stringify({product: productJson, order: []}, null, '  '));
+};
+
+utils.numberToHour = function(number){
+  return (Math.floor(number) % 24) + ':' + (number % 1 * 60 + '').padStart(2, '0');
+};
+
+utils.hourToNumber = function(hour){
+  const parts = hour.split(':');
+
+  return parseInt(parts[0]) + parseInt(parts[1])/60;
+};
+
+utils.dateToStr = function(dateObj){
+  return dateObj.toISOString().slice(0, 10);
+};
+
+utils.addDays = function(dateStr, days){
+  const dateObj = new Date(dateStr);
+  dateObj.setDate(dateObj.getDate() + days);
+  return dateObj;
+};
+
 Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
   return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
 });
@@ -49,6 +78,3 @@ Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
 Handlebars.registerHelper('joinValues', function(input, options) {
   return Object.values(input).join(options.fn(this));
 });
-
-
-export default utils;
