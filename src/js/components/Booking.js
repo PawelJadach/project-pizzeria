56dc01990c0ab08b34a1c2439a1765a4f112226f
Paweł Jadach <pawel.jadach.96@gmail.com>
2019-10-26 12:03:51 +0200

    Add tables booked

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,4 @@
-import { templates, select, settings } from '../settings.js';
+import { templates, select, settings, classNames } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
 import utils from '../utils.js';
 import DatePicker from './DatePicker.js';
@@ -35,6 +35,9 @@ class Booking {
     thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(
       select.widgets.hourPicker.wrapper
     );
+    thisBooking.dom.tables = thisBooking.dom.wrapper.querySelectorAll(
+      select.booking.tables
+    );
   }
 
   initWidgets() {
@@ -43,6 +46,10 @@ class Booking {
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
     thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
     thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+
+    thisBooking.dom.wrapper.addEventListener('updated', function() {
+      thisBooking.updateDOM();
+    });
   }
 
   getData() {
@@ -82,7 +89,7 @@ class Booking {
         '?' +
         params.eventsRepeat.join('&')
     };
-
+    //console.log(urls);
     Promise.all([
       fetch(urls.booking),
       fetch(urls.eventsCurrent),
@@ -99,9 +106,9 @@ class Booking {
         ]);
       })
       .then(function([bookingRes, eventsCurrentRes, eventsRepeatRes]) {
-        // console.log(bookingRes);
+        //console.log('bookingRes', bookingRes);
         // console.log(eventsCurrentRes);
-        // console.log(eventsRepeatRes);
+        //console.log(eventsRepeatRes);
 
         thisBooking.parseData(bookingRes, eventsCurrentRes, eventsRepeatRes);
       });
@@ -109,26 +116,97 @@ class Booking {
 
   parseData(bookings, eventsCurrent, eventsRepeat) {
     const thisBooking = this;
-
+    //console.log(eventsRepeat);
     thisBooking.booked = {};
     for (let item of eventsCurrent) {
       thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
     }
-    console.log(thisBooking.booked);
+
+    for (let item of bookings) {
+      thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
+    }
+
+    const minDate = thisBooking.datePicker.minDate;
+    const maxDate = thisBooking.datePicker.maxDate;
+
+    for (let item of eventsRepeat) {
+      if (item.repeat == 'daily') {
+        //console.log('git');
+        for (
+          let loopDate = minDate;
+          loopDate <= maxDate;
+          loopDate = utils.addDays(loopDate, 1)
+        ) {
+          thisBooking.makeBooked(
+            utils.dateToStr(loopDate),
+            item.hour,
+            item.duration,
+            item.table
+          );
+        }
+      }
+    }
+
+    //console.log(thisBooking.booked);
+    thisBooking.updateDOM();
   }
 
   makeBooked(date, hour, duration, table) {
-    if (typeof this.booked[date] == 'undefined') {
-      this.booked[date] = {};
+    const thisBooking = this;
+    if (typeof thisBooking.booked[date] == 'undefined') {
+      thisBooking.booked[date] = {};
     }
 
     const startHour = utils.hourToNumber(hour);
 
-    if (typeof this.booked[date][startHour] == 'undefined') {
-      this.booked[date][startHour] = [];
-    }
+    for (
+      let hourBlock = startHour;
+      hourBlock < startHour + duration;
+      hourBlock += 0.5
+    ) {
+      if (typeof thisBooking.booked[date][hourBlock] == 'undefined') {
+        thisBooking.booked[date][hourBlock] = [];
+      }
 
-    this.booked[date][startHour].push(table);
+      thisBooking.booked[date][hourBlock].push(table);
+    }
+  }
+
+  updateDOM() {
+    const thisBooking = this;
+    console.log(thisBooking.datePicker);
+    thisBooking.date = thisBooking.datePicker.value;
+    thisBooking.hour = utils.hourToNumber(thisBooking.hourPicker.value);
+    console.log('thisBooking.hour', thisBooking.hour);
+    console.log('thisBooking.date', thisBooking.date);
+    let allAvailable = false;
+
+    if (
+      typeof thisBooking.booked[thisBooking.date] == 'undefined' ||
+      typeof thisBooking.booked[thisBooking.date][thisBooking.hour] ==
+        'undefined'
+    ) {
+      //console.log('zmieniam');
+      allAvailable = true;
+    }
+    //console.log(thisBooking.dom.tables);
+    for (let table of thisBooking.dom.tables) {
+      let tableId = table.getAttribute(settings.booking.tableIdAttribute);
+      if (!isNaN(tableId)) {
+        tableId = parseInt(tableId);
+      }
+      //console.log(allAvailable);
+      if (
+        !allAvailable &&
+        thisBooking.booked[thisBooking.date][thisBooking.hour].includes(tableId)
+      ) {
+        console.log('daje');
+        table.classList.add(classNames.booking.tableBooked);
+      } else {
+        //console.log('zabieram');
+        table.classList.remove(classNames.booking.tableBooked);
+      }
+    }
   }
 }
 
--- src/js/components/DatePicker.js
+++ src/js/components/DatePicker.js
@@ -5,46 +5,48 @@ import { settings, select } from '../settings.js';
 // import flatpickr from 'flatpickr';
 
 class DatePicker extends BaseWidget {
-  constructor(wrapper){
-    
+  constructor(wrapper) {
     super(wrapper);
     const thisWidget = this;
     utils.dateToStr(new Date());
-    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(
+      select.widgets.datePicker.input
+    );
     thisWidget.initPlugin();
   }
 
-  initPlugin(){
+  initPlugin() {
     const thisWidget = this;
     thisWidget.minDate = new Date();
-    thisWidget.maxDate = utils.addDays(this.minDate, settings.datePicker.maxDaysInFuture);
+    thisWidget.maxDate = utils.addDays(
+      this.minDate,
+      settings.datePicker.maxDaysInFuture
+    );
     //console.log(thisWidget.minDate, thisWidget.maxDate);
     flatpickr(thisWidget.dom.input, {
       defaultDate: thisWidget.minDate,
       minDate: thisWidget.minDate,
       maxDate: thisWidget.maxDate,
       locale: {
-        'firstDayOfWeek': 1 // start week on Monday
-      } ,
+        firstDayOfWeek: 1 // start week on Monday
+      },
       disable: [
         function(date) {
           // return true to disable
-          return (date.getDay() === 1);
+          return date.getDay() === 1;
         }
-      ],
+      ]
     });
   }
-  parseValue(value){
+  parseValue(value) {
     return value;
   }
 
-  isValid(){
+  isValid() {
     return true;
   }
 
-  renderValue(){
-   
-  }
+  renderValue() {}
 }
 
 export default DatePicker;
